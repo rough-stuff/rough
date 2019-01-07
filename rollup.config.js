e208117def26512b21b90af1ae7bd527375c98c7
Preet Shihn <preetshihn@gmail.com>
2019-01-06 23:30:41 -0800

    .

--- rollup.config.js
+++ rollup.config.js
@@ -21,6 +21,60 @@ export default [
     plugins: [terser()]
   },
   {
+    input: 'bin/wrappers/rough-canvas.js',
+    output: {
+      file: 'dist/rough-canvas.js',
+      format: 'iife',
+      name: 'rough'
+    },
+    plugins: [terser()]
+  },
+  {
+    input: 'bin/wrappers/rough-svg.js',
+    output: {
+      file: 'dist/rough-svg.js',
+      format: 'iife',
+      name: 'rough'
+    },
+    plugins: [terser()]
+  },
+  {
+    input: 'bin/wrappers/rough-async.js',
+    output: {
+      file: 'dist/rough-async.js',
+      format: 'iife',
+      name: 'rough'
+    },
+    plugins: [resolve(), terser()]
+  },
+  {
+    input: 'bin/wrappers/rough-async.js',
+    output: {
+      file: 'dist/rough-async.umd.js',
+      format: 'umd',
+      name: 'rough'
+    },
+    plugins: [resolve(), terser()]
+  },
+  {
+    input: 'bin/wrappers/rough-canvas-async.js',
+    output: {
+      file: 'dist/rough-canvas-async.js',
+      format: 'iife',
+      name: 'rough'
+    },
+    plugins: [resolve(), terser()]
+  },
+  {
+    input: 'bin/wrappers/rough-svg-async.js',
+    output: {
+      file: 'dist/rough-svg-async.js',
+      format: 'iife',
+      name: 'rough'
+    },
+    plugins: [resolve(), terser()]
+  },
+  {
     input: 'bin/wrappers/worker.js',
     output: {
       file: 'dist/worker.js',
