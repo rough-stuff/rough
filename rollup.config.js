1eec028ec7394b852bd21b43027e789789e6ca92
Preet Shihn <preetshihn@gmail.com>
2019-01-07 00:29:05 -0800

    es5 build

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve';
 import { terser } from "rollup-plugin-terser";
+const typescript = require('rollup-plugin-typescript');
 
 export default [
   {
@@ -46,5 +47,52 @@ export default [
       name: 'roughWorker'
     },
     plugins: [resolve(), terser()]
+  },
+
+  // es5
+  {
+    input: 'src/wrappers/rough.ts',
+    output: {
+      file: 'dist/rough.es5.js',
+      format: 'iife',
+      name: 'rough'
+    },
+    plugins: [typescript({ target: "es5" }), terser()]
+  },
+  {
+    input: 'src/wrappers/rough.ts',
+    output: {
+      file: 'dist/rough.es5.umd.js',
+      format: 'umd',
+      name: 'rough'
+    },
+    plugins: [typescript({ target: "es5" }), terser()]
+  },
+  {
+    input: 'src/wrappers/rough-async.ts',
+    output: {
+      file: 'dist/rough-async.es5.js',
+      format: 'iife',
+      name: 'rough'
+    },
+    plugins: [typescript({ target: "es5" }), terser()]
+  },
+  {
+    input: 'src/wrappers/rough-async.ts',
+    output: {
+      file: 'dist/rough-async.es5.umd.js',
+      format: 'umd',
+      name: 'rough'
+    },
+    plugins: [typescript({ target: "es5" }), terser()]
+  },
+  {
+    input: 'src/wrappers/worker.ts',
+    output: {
+      file: 'dist/worker.es5.js',
+      format: 'iife',
+      name: 'roughWorker'
+    },
+    plugins: [typescript({ target: "es5" }), terser()]
   }
 ];
