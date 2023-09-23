e780eb591e0f354bc134273872445cf3489d5e87
Preet <833927+pshihn@users.noreply.github.com>
2023-09-23 11:57:03 -0700

    use hachure-fill lib

--- src/geometry.ts
+++ src/geometry.ts
@@ -8,26 +8,6 @@ export interface Rectangle {
   height: number;
 }
 
-export function rotatePoints(points: Point[], center: Point, degrees: number): void {
-  if (points && points.length) {
-    const [cx, cy] = center;
-    const angle = (Math.PI / 180) * degrees;
-    const cos = Math.cos(angle);
-    const sin = Math.sin(angle);
-    points.forEach((p) => {
-      const [x, y] = p;
-      p[0] = ((x - cx) * cos) - ((y - cy) * sin) + cx;
-      p[1] = ((x - cx) * sin) + ((y - cy) * cos) + cy;
-    });
-  }
-}
-
-export function rotateLines(lines: Line[], center: Point, degrees: number): void {
-  const points: Point[] = [];
-  lines.forEach((line) => points.push(...line));
-  rotatePoints(points, center, degrees);
-}
-
 export function lineLength(line: Line): number {
   const p1 = line[0];
   const p2 = line[1];
