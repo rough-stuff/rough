bcbeb442d177d1951252fbc80c5379aa58ddeefa
Benji Visser <benji@093b.org>
2024-03-19 00:26:52 +0000

    Upgrade dependencies

--- src/generator.ts
+++ src/generator.ts
@@ -154,7 +154,7 @@ export class RoughGenerator {
                 points[0],
                 points[1],
                 points[2],
-              ]).map(p => [p[0], p[1]] as Point), 10, (1 + o.roughness) / 2));
+              ]).map(p => [p[0], p[1]] as Point), 10, (1 + o.roughness) / 2).map(p => [p[0], p[1]] as Point));
             } else {
               polyPoints.push(...pointsOnBezierCurves(curveToBezier(points), 10, (1 + o.roughness) / 2));
             }
